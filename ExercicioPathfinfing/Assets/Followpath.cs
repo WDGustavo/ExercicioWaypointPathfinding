7c0cf33ef0ae8d7d2d67bcd720e8b6fb1b6cdf3c
WDGustavo <54276455+WDGustavo@users.noreply.github.com>
2021-03-31 23:46:19 -0300

    CodigoComentadoEfuncional

---
+++ ExercicioPathfinfing/Assets/Followpath.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Followpath : MonoBehaviour
+{
+    //Olha para o Waypoint
+    Transform goal;
+    //Seta a velocidade do tanque
+    public float speed = 5.0f;
+    //Seta a accuracy do tanque
+    public float accuracy = 1.0f;
+    //Seta a suavidade da rotação do tanque
+    public float rotSpeed = 2.0f;
+
+    //Setar o objeto que tem o codigo do WPManager
+    public GameObject wpManager;
+    //Pega a posição do waypoint a lista
+    GameObject[] wps;
+    //Links
+    GameObject currentNode;
+    //Waypoint atual
+    int currentWP = 0;
+    //Pega os componentes do codigo graph
+    Graph g;
+
+    void Start()
+    {
+        //Pega os componentes do WPManager
+        wps = wpManager.GetComponent<WPManager>().waypoints;
+        g = wpManager.GetComponent<WPManager>().graph;
+        currentNode = wps[0];
+    }
+
+    //Função para levar uma posição especifica no caso o heliporto
+    public void GoToHeli()
+    {
+        g.AStar(currentNode, wps[1]);
+        currentWP = 0;
+    }
+
+    //Função para levar uma posição especifica no caso as ruinas
+    public void GoToRuin()
+    {
+        g.AStar(currentNode, wps[5]);
+        currentWP = 0;
+    }
+
+    void LateUpdate()
+    {
+        //Se a posição do waypoint for 0 fique parado
+
+        if (g.getPathLength() == 0 || currentWP == g.getPathLength())
+            return;
+
+        //Waypoint mais proximo
+        currentNode = g.getPathPoint(currentWP);
+
+        //Ao chegar em um waypoint vá para o waypoint mais proximo
+        if (Vector3.Distance(g.getPathPoint(currentWP).transform.position, transform.position) < accuracy)
+        {
+            currentWP++;
+        }
+
+        //Movimentação para outro waypoint
+        if (currentWP < g.getPathLength())
+        {
+            //faz o tanque ir para um waypoint proximo
+            goal = g.getPathPoint(currentWP).transform;
+            Vector3 lookAtGoal = new Vector3(goal.position.x, this.transform.position.y, goal.position.z);
+            Vector3 direction = lookAtGoal - this.transform.position;
+            //Ajuda a suavisar a rotação
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime * rotSpeed);
+        }
+        //Faz o player se mover
+        transform.Translate(0, 0, speed * Time.deltaTime);
+    }
+}
---
+++ ExercicioPathfinfing/Assets/WPManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Faz as variaveis aparecerem no inspector
+[System.Serializable]
+
+public struct Link
+{
+    //Cria o tipo de direção dos links do waypoint UNI so tem uma direção e BI pode ir e voltar pelo link
+    public enum direction {UNI, BI}
+    //Seta os links entre os waypoints
+    public GameObject node1;
+    public GameObject node2;
+    //Seta a direção dos links
+    public direction dir;
+}
+
+public class WPManager : MonoBehaviour
+{
+    //Cria uma lista de Waypoints
+    public GameObject[] waypoints;
+    //Cria uma lista de Links
+    public Link[] links;
+    //Pega informações do codigo Graph
+    public Graph graph = new Graph();
+
+    void Start()
+    {
+        //Quando a lista estiver maior que 0 faça os loops
+        if(waypoints.Length > 0)
+        {
+            //Faz um loop dos Waypoints
+            foreach(GameObject wp in waypoints)
+            {
+                //Adiciona um novo Waypoint
+                graph.AddNode(wp);
+            }
+            //Faz o loop de Links
+            foreach(Link l in links)
+            {
+                graph.AddEdge(l.node1, l.node2);
+                //Caso o link tenha a direção BI faz a condição dele ir e voltar
+                if (l.dir == Link.direction.BI)
+                    graph.AddEdge(l.node2, l.node1);
+            }
+        }
+    }
+
+    void Update()
+    {
+        //Mostra as linhas do Link
+        graph.debugDraw(); 
+    }
+}
