557a9c36413ba5013f67d0be7b3d2b8f0a5e29ca
WDGustavo <54276455+WDGustavo@users.noreply.github.com>
2021-04-14 14:49:20 -0300

    Navmesh
    adição de movimentação utilizando o navmesh

--- ExercicioPathfinfing/Assets/Followpath.cs
+++ ExercicioPathfinfing/Assets/Followpath.cs
@@ -6,61 +6,92 @@ public class Followpath : MonoBehaviour
 {
     //Olha para o Waypoint
     Transform goal;
+
+    /* usado no waypoint sem nav mesh
     //Seta a velocidade do tanque
     public float speed = 5.0f;
     //Seta a accuracy do tanque
     public float accuracy = 1.0f;
     //Seta a suavidade da rotação do tanque
-    public float rotSpeed = 2.0f;
+    public float rotSpeed = 2.0f;*/
+
     //Setar o objeto que tem o codigo do WPManager
     public GameObject wpManager;
     //Pega a posição do waypoint a lista
     GameObject[] wps;
+    /* usado no waypoint sem nav mesh
     //Links
     GameObject currentNode;
     //Waypoint atual
     int currentWP = 0;
     //Pega os componentes do codigo graph
-    Graph g;
+    Graph g;*/
+
+    //Variavel do nav mesh agent
+    UnityEngine.AI.NavMeshAgent agent;
 
     void Start()
     {
         //Pega os componentes do WPManager
         wps = wpManager.GetComponent<WPManager>().waypoints;
+        //pega as informações do navmesh
+        agent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
+
+        /* usado no waypoint sem nav mesh
         g = wpManager.GetComponent<WPManager>().graph;
         //Seta o waypoint atual no element 0
-        currentNode = wps[0];
-    }
+        currentNode = wps[0];*/
 
-    //Função para levar uma posição especifica no caso o heliporto
-    public void GoToHeli()
+}
+
+//Função para levar uma posição especifica no caso o heliporto
+public void GoToHeli()
     {
+        //seta o local usando o navmesh
+        agent.SetDestination(wps[1].transform.position);
+
+        /* usado no waypoint sem nav mesh
         //Seta o local da lista que o tanque deve parar
         g.AStar(currentNode, wps[1]);
         //Ao chegar no ponto a lista volta para 0
-        currentWP = 0;
+        currentWP = 0;*/
+
     }
 
     //Função para levar uma posição especifica no caso as ruinas
     public void GoToRuin()
     {
+        //seta o local usando o navmesh
+        agent.SetDestination(wps[5].transform.position);
+
+        /* usado no waypoint sem nav mesh
         //Seta o local da lista que o tanque deve parar
         g.AStar(currentNode, wps[5]);
         //Ao chegar no ponto a lista volta para 0
-        currentWP = 0;
+        currentWP = 0;*/
+
     }
 
     //Função para levar uma posição especifica no caso a industria
     public void GoToIndustria()
     {
+        //seta o local usando o navmesh
+        agent.SetDestination(wps[10].transform.position);
+
+        /* usado no waypoint sem nav mesh
         //Seta o local da lista que o tanque deve parar
         g.AStar(currentNode, wps[10]);
         //Ao chegar no ponto a lista volta para 0
-        currentWP = 0;
+        currentWP = 0;*/
+
     }
 
     void LateUpdate()
     {
+
+
+
+        /* usado no waypoint sem nav mesh
         //Se a posição do waypoint for 0 fique parado
         if (g.getPathLength() == 0 || currentWP == g.getPathLength())
             return;
@@ -85,6 +116,6 @@ public class Followpath : MonoBehaviour
             this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime * rotSpeed);
         }
         //Faz o tanque se mover
-        transform.Translate(0, 0, speed * Time.deltaTime);
+        transform.Translate(0, 0, speed * Time.deltaTime);*/
     }
 }
